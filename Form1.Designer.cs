3e6d0c8c4f51a46186b39e9e4c5b7c39df6da834
GlinskiiIvan <glinsky_ivan@mail.ru>
2021-09-22 10:47:15 +0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace testCharts
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox_chart = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_a = new System.Windows.Forms.Label();
+            this.label_b = new System.Windows.Forms.Label();
+            this.label_c = new System.Windows.Forms.Label();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.textBox_b = new System.Windows.Forms.TextBox();
+            this.textBox_c = new System.Windows.Forms.TextBox();
+            this.button_createChart = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_chart
+            // 
+            this.pictureBox_chart.Location = new System.Drawing.Point(57, 22);
+            this.pictureBox_chart.Name = "pictureBox_chart";
+            this.pictureBox_chart.Size = new System.Drawing.Size(780, 480);
+            this.pictureBox_chart.TabIndex = 0;
+            this.pictureBox_chart.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 530);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(286, 530);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label_a
+            // 
+            this.label_a.AutoSize = true;
+            this.label_a.Location = new System.Drawing.Point(369, 552);
+            this.label_a.Name = "label_a";
+            this.label_a.Size = new System.Drawing.Size(13, 15);
+            this.label_a.TabIndex = 3;
+            this.label_a.Text = "a";
+            // 
+            // label_b
+            // 
+            this.label_b.AutoSize = true;
+            this.label_b.Location = new System.Drawing.Point(369, 581);
+            this.label_b.Name = "label_b";
+            this.label_b.Size = new System.Drawing.Size(14, 15);
+            this.label_b.TabIndex = 4;
+            this.label_b.Text = "b";
+            // 
+            // label_c
+            // 
+            this.label_c.AutoSize = true;
+            this.label_c.Location = new System.Drawing.Point(369, 610);
+            this.label_c.Name = "label_c";
+            this.label_c.Size = new System.Drawing.Size(13, 15);
+            this.label_c.TabIndex = 5;
+            this.label_c.Text = "c";
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Location = new System.Drawing.Point(389, 543);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(100, 23);
+            this.textBox_a.TabIndex = 6;
+            // 
+            // textBox_b
+            // 
+            this.textBox_b.Location = new System.Drawing.Point(389, 572);
+            this.textBox_b.Name = "textBox_b";
+            this.textBox_b.Size = new System.Drawing.Size(100, 23);
+            this.textBox_b.TabIndex = 7;
+            // 
+            // textBox_c
+            // 
+            this.textBox_c.Location = new System.Drawing.Point(389, 601);
+            this.textBox_c.Name = "textBox_c";
+            this.textBox_c.Size = new System.Drawing.Size(100, 23);
+            this.textBox_c.TabIndex = 8;
+            // 
+            // button_createChart
+            // 
+            this.button_createChart.Location = new System.Drawing.Point(369, 651);
+            this.button_createChart.Name = "button_createChart";
+            this.button_createChart.Size = new System.Drawing.Size(120, 23);
+            this.button_createChart.TabIndex = 9;
+            this.button_createChart.Text = "Построить";
+            this.button_createChart.UseVisualStyleBackColor = true;
+            this.button_createChart.Click += new System.EventHandler(this.button_createChart_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(229, 562);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 705);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button_createChart);
+            this.Controls.Add(this.textBox_c);
+            this.Controls.Add(this.textBox_b);
+            this.Controls.Add(this.textBox_a);
+            this.Controls.Add(this.label_c);
+            this.Controls.Add(this.label_b);
+            this.Controls.Add(this.label_a);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox_chart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_chart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_a;
+        private System.Windows.Forms.Label label_b;
+        private System.Windows.Forms.Label label_c;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.TextBox textBox_b;
+        private System.Windows.Forms.TextBox textBox_c;
+        private System.Windows.Forms.Button button_createChart;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testCharts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = pictureBox_chart.CreateGraphics();
+            Pen pen = new Pen(Color.Red, 2);
+
+            // Размеры области построения графика
+            int width = pictureBox_chart.Width;
+            int height = pictureBox_chart.Height;
+
+            // Координатные оси
+            graphics.DrawLine(pen, width / 2, 0, width / 2, height);
+            graphics.DrawLine(pen, 0, height / 2, width, height / 2);
+
+            // Стрелки координатных осей
+            graphics.DrawLine(pen, width / 2, 0, width / 2 + 5, 10);
+            graphics.DrawLine(pen, width / 2, 0, width / 2 - 5, 10);
+            graphics.DrawLine(pen, width, height / 2, width - 10, height / 2 + 5);
+            graphics.DrawLine(pen, width, height / 2, width - 10, height / 2 - 5);
+
+            // Подписи координатных осей
+            graphics.DrawString("Y", new Font("10_IC_1", 12), Brushes.Black, width / 2 + 10, 0);
+            graphics.DrawString("X", new Font("10_IC_1", 12), Brushes.Black, width-15, height / 2 + 10); //width, height / 2
+
+
+            // Координатная сетка и отметки на координатных осях
+            int lx = -width / 20;
+            for (int i = 10; i < width; i += 10)
+            {
+                graphics.DrawLine(new Pen(Color.Black, 2), 0+i, height/2-5, 0+i, height/2+5); // метка на оси OX
+                //graphics.DrawString(lx.ToString(), new Font("10_IC_1", 7), Brushes.Black, 0 + i -5, height / 2 + 5); // подписи меток
+                //graphics.DrawLine(new Pen(Color.Gray, 1), 0 + i, 0, 0 + i, height); // Горизонтальная линия сетки
+                ++lx;
+            }
+            int ly = height / 20;
+            for (int i = 10; i < height; i += 10)
+            {
+                graphics.DrawLine(new Pen(Color.Black, 2), width / 2 - 5, 0 + i, width / 2 + 5, 0 + i); // метка на оси OY
+                //graphics.DrawString(ly.ToString(), new Font("10_IC_1", 7), Brushes.Black, width / 2 + 5, 0 + i - 5); // подписи меток
+                //graphics.DrawLine(new Pen(Color.Gray, 1), 0, 0 + i, width, 0 + i); // Вертикальная линия сетки
+                --ly;
+            }
+
+            label1.Text = width.ToString();
+            label2.Text = height.ToString();
+        }
+
+        private void button_createChart_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = pictureBox_chart.CreateGraphics();
+            Pen pen = new Pen(Color.Red, 2);
+
+            int width = pictureBox_chart.Width;
+            int height = pictureBox_chart.Height;
+
+            double a = Convert.ToDouble(textBox_a.Text);
+            double b = Convert.ToDouble(textBox_b.Text);
+            double c = Convert.ToDouble(textBox_c.Text);
+
+            double D = (int)Math.Pow(b, 2) - 4 * a * c;
+
+            double x0 = -(b / (2 * a));
+            double y0 = -(D / (4 * a));           
+
+            label3.Text = D.ToString() + " " + x0.ToString() + " " + y0.ToString();
+
+            // y = x^2
+            PointF[] points = new PointF[1000];
+            int idx = 0;
+
+            if (a == 0)
+            {
+                for (int i = -points.Length / 2; i < points.Length / 2; i++)
+                {
+                    points[idx] = new PointF(i + width / 2, -(float)(a * Math.Pow(i, 2) + b * i + c) + height / 2 + -(float)c*10);
+                    ++idx;
+                }
+            } else
+            {
+                for (int i = -points.Length / 2; i < points.Length / 2; i++)
+                {
+                    //points[idx] = new Point(i + width/2 - (int)x0, -(int)Math.Pow(i, 2) / 50 + height/2 + -((int)y0));
+                    //points[idx] = new PointF(i + width / 2 + (float)x0*10, -(float)Math.Pow(i, 2) / 10 + height / 2 + -(float)y0*10);
+                    points[idx] = new PointF(i + width / 2 + (float)x0 * 10, -(float)(a * Math.Pow(i, 2) + b * i + c) / 10 + height / 2 + -(float)y0 * 10);
+
+                    ++idx;
+                }
+            }
+
+            graphics.DrawLines(pen, points);
+        }
+    }
+}
