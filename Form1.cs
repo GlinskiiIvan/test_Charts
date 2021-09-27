b93aa225af4b76e5ae922fc4451626633b64efd7
GlinskiiIvan <glinsky_ivan@mail.ru>
2021-09-27 12:11:10 +0600

    event changed

--- Form1.cs
+++ Form1.cs
@@ -20,7 +20,54 @@ namespace testCharts
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            
+            Graphics graphics = pictureBox_chart.CreateGraphics();
+            Pen pen = new Pen(Color.Red, 2);
+
+            // Размеры области построения графика
+            int width = pictureBox_chart.Width;
+            int height = pictureBox_chart.Height;
+
+            // Координатные оси
+            graphics.DrawLine(pen, width / 2, 0, width / 2, height);
+            graphics.DrawLine(pen, 0, height / 2, width, height / 2);
+
+            // Стрелки координатных осей
+            graphics.DrawLine(pen, width / 2, 0, width / 2 + 5, 10);
+            graphics.DrawLine(pen, width / 2, 0, width / 2 - 5, 10);
+            graphics.DrawLine(pen, width, height / 2, width - 10, height / 2 + 5);
+            graphics.DrawLine(pen, width, height / 2, width - 10, height / 2 - 5);
+
+            // Подписи координатных осей
+            graphics.DrawString("Y", new Font("10_IC_1", 12), Brushes.Black, width / 2 + 10, -5);
+            graphics.DrawString("X", new Font("10_IC_1", 12), Brushes.Black, width - 15, height / 2 + 10); //width, height / 2
+
+
+            // Координатная сетка и отметки на координатных осях
+            int lx = -width / 40 + 1;
+            for (int i = 20; i < width - 20; i += 20)
+            {
+                if (lx != 0)
+                {
+                    graphics.DrawLine(new Pen(Color.Gray, 1), 0 + i + 10, 0, 0 + i + 10, height); // Горизонтальная линия сетки
+                    graphics.DrawString(lx.ToString(), new Font("10_IC_1", 8, FontStyle.Bold), Brushes.Black, 0 + i + 5, height / 2 + 5); // подписи меток
+                }
+                graphics.DrawLine(new Pen(Color.Black, 2), 0 + i + 10, height / 2 - 5, 0 + i + 10, height / 2 + 5); // метка на оси OX
+
+                ++lx;
+            }
+
+            int ly = height / 40 - 1;
+            for (int i = 20; i < height; i += 20)
+            {
+                if (ly != 0)
+                {
+                    graphics.DrawLine(new Pen(Color.Gray, 1), 0, 0 + i, width, 0 + i); // Вертикальная линия сетки
+                    graphics.DrawString(ly.ToString(), new Font("10_IC_1", 8, FontStyle.Bold), Brushes.Black, width / 2 + 5, 0 + i - 5); // подписи меток
+                }
+                graphics.DrawLine(new Pen(Color.Black, 2), width / 2 - 5, 0 + i, width / 2 + 5, 0 + i); // метка на оси OY
+
+                --ly;
+            }
         }
 
         private void button_createChart_Click(object sender, EventArgs e)
@@ -85,54 +132,7 @@ namespace testCharts
 
         private void pictureBox_chart_Layout(object sender, LayoutEventArgs e)
         {
-            Graphics graphics = pictureBox_chart.CreateGraphics();
-            Pen pen = new Pen(Color.Red, 2);
-
-            // Размеры области построения графика
-            int width = pictureBox_chart.Width;
-            int height = pictureBox_chart.Height;
-
-            // Координатные оси
-            graphics.DrawLine(pen, width / 2, 0, width / 2, height);
-            graphics.DrawLine(pen, 0, height / 2, width, height / 2);
-
-            // Стрелки координатных осей
-            graphics.DrawLine(pen, width / 2, 0, width / 2 + 5, 10);
-            graphics.DrawLine(pen, width / 2, 0, width / 2 - 5, 10);
-            graphics.DrawLine(pen, width, height / 2, width - 10, height / 2 + 5);
-            graphics.DrawLine(pen, width, height / 2, width - 10, height / 2 - 5);
-
-            // Подписи координатных осей
-            graphics.DrawString("Y", new Font("10_IC_1", 12), Brushes.Black, width / 2 + 10, -5);
-            graphics.DrawString("X", new Font("10_IC_1", 12), Brushes.Black, width - 15, height / 2 + 10); //width, height / 2
-
-
-            // Координатная сетка и отметки на координатных осях
-            int lx = -width / 40 + 1;
-            for (int i = 20; i < width - 20; i += 20)
-            {
-                if (lx != 0)
-                {
-                    graphics.DrawLine(new Pen(Color.Gray, 1), 0 + i + 10, 0, 0 + i + 10, height); // Горизонтальная линия сетки
-                    graphics.DrawString(lx.ToString(), new Font("10_IC_1", 8, FontStyle.Bold), Brushes.Black, 0 + i + 5, height / 2 + 5); // подписи меток
-                }
-                graphics.DrawLine(new Pen(Color.Black, 2), 0 + i + 10, height / 2 - 5, 0 + i + 10, height / 2 + 5); // метка на оси OX
-
-                ++lx;
-            }
-
-            int ly = height / 40 - 1;
-            for (int i = 20; i < height; i += 20)
-            {
-                if (ly != 0)
-                {
-                    graphics.DrawLine(new Pen(Color.Gray, 1), 0, 0 + i, width, 0 + i); // Вертикальная линия сетки
-                    graphics.DrawString(ly.ToString(), new Font("10_IC_1", 8, FontStyle.Bold), Brushes.Black, width / 2 + 5, 0 + i - 5); // подписи меток
-                }
-                graphics.DrawLine(new Pen(Color.Black, 2), width / 2 - 5, 0 + i, width / 2 + 5, 0 + i); // метка на оси OY
-
-                --ly;
-            }
+            
         }
     }
 }
